5552f9623392d675b9dfd7941fb401a5ac85f6e6
Mark Moxon <mark@moxon.net>
2026-06-02 12:20:36 +0100

    Rename line-drawing deep dive

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -45473,7 +45473,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a line: Calculate the line gradient in the form of deltas
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;
 ; ------------------------------------------------------------------------------
 ;
@@ -45577,7 +45577,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a line: Line has a shallow gradient, step right along x-axis
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;
 ; ------------------------------------------------------------------------------
 ;
@@ -45713,7 +45713,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a shallow line going right and up or left and down
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;
 ; ------------------------------------------------------------------------------
 ;
@@ -46162,7 +46162,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a shallow line going right and down or left and up
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;
 ; ------------------------------------------------------------------------------
 ;
@@ -46648,7 +46648,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a line: Line has a steep gradient, step up along y-axis
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;
 ; ------------------------------------------------------------------------------
 ;
@@ -46811,7 +46811,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a steep line going up and left or down and right
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;
 ; ------------------------------------------------------------------------------
 ;
@@ -46919,7 +46919,7 @@ ENDIF
 ;       Type: Subroutine
 ;   Category: Drawing lines
 ;    Summary: Draw a steep line going up and right or down and left
-;  Deep dive: Bresenham's line algorithm
+;  Deep dive: Elite's line-drawing algorithm
 ;
 ; ------------------------------------------------------------------------------
 ;
